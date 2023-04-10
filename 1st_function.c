64fc18004735be3bbf28255bd207dbbed2fca4b8
Manu0990 <130347541+Manu0990@users.noreply.github.com>
2023-04-10 13:07:16 +0530

    My first upload!

---
+++ 1st_function.c
@@ -0,0 +1,18 @@
+//With argument and with return value.
+
+
+
+#include<stdio.h>
+int sum(int a, int b)
+{return a+b;}
+int mul(int a,int b)
+{return a*b;}
+int main(int argc, char const *argv[])
+{
+    int a=5,b=5,c,d;
+    d=sum(a,b);
+    c=mul(a,b);
+    printf("The sum of a & b is %d\n",d);
+    printf("The mul of a & b is %d",c);
+    return 0;
+}
